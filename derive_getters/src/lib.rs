a702184d35fe35692612ecffd984b31150276ed4
Jonas Platte <jplatte+git@posteo.de>
2021-05-11 12:37:45 +0200

    Error handling: Registering the attribute

--- derive_getters/src/lib.rs
+++ derive_getters/src/lib.rs
@@ -4,7 +4,7 @@ use syn::{parse_macro_input, DeriveInput};
 mod getters;
 use getters::expand_getters;
 
-#[proc_macro_derive(Getters)]
+#[proc_macro_derive(Getters, attributes(getter))]
 pub fn getters(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     expand_getters(input).into()
