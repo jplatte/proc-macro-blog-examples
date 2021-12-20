cf70387268c2d6feb8e69af54b42319589729dff
Jonas Platte <jplatte+git@posteo.de>
2021-12-20 19:19:17 +0100

    Parsing custom syntax: Branching

--- derive_getters/src/getters.rs
+++ derive_getters/src/getters.rs
@@ -1,8 +1,8 @@
 use proc_macro2::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{
     parse::{Parse, ParseStream},
-    Attribute, Data, DataStruct, DeriveInput, Fields, Ident, Token,
+    parse_quote, Data, DataStruct, DeriveInput, Fields, Ident, Token, Visibility,
 };
 
 pub fn expand_getters(input: DeriveInput) -> syn::Result<TokenStream> {
@@ -14,28 +14,19 @@ pub fn expand_getters(input: DeriveInput) -> syn::Result<TokenStream> {
     let getters = fields
         .into_iter()
         .map(|f| {
-            let attrs: Vec<_> =
-                f.attrs.iter().filter(|attr| attr.path.is_ident("getter")).collect();
+            let meta: GetterMeta = f
+                .attrs
+                .iter()
+                .filter(|attr| attr.path.is_ident("getter"))
+                .try_fold(GetterMeta::default(), |meta, attr| meta.merge(attr.parse_args()?))?;
 
-            let name_from_attr = match attrs.len() {
-                0 => None,
-                1 => get_name_attr(attrs[0])?,
-                _ => {
-                    let mut error =
-                        syn::Error::new_spanned(attrs[1], "redundant `getter(name)` attribute");
-                    error.combine(syn::Error::new_spanned(attrs[0], "note: first one here"));
-                    return Err(error);
-                }
-            };
-
-            // if there is no `getter(name)` attribute use the field name like before
-            let method_name =
-                name_from_attr.unwrap_or_else(|| f.ident.clone().expect("a named field"));
+            let visibility = meta.vis.unwrap_or_else(|| parse_quote! { pub });
+            let method_name = meta.name.unwrap_or_else(|| f.ident.clone().expect("a named field"));
             let field_name = f.ident;
             let field_ty = f.ty;
 
             Ok(quote! {
-                pub fn #method_name(&self) -> &#field_ty {
+                #visibility fn #method_name(&self) -> &#field_ty {
                     &self.#field_name
                 }
             })
@@ -53,28 +44,48 @@ pub fn expand_getters(input: DeriveInput) -> syn::Result<TokenStream> {
     })
 }
 
-fn get_name_attr(attr: &Attribute) -> syn::Result<Option<Ident>> {
-    let meta: GetterMeta = attr.parse_args()?;
-    Ok(meta.name)
-}
-
+#[derive(Default)]
 struct GetterMeta {
     name: Option<Ident>,
+    vis: Option<Visibility>,
+}
+
+impl GetterMeta {
+    fn merge(self, other: GetterMeta) -> syn::Result<Self> {
+        fn either<T: ToTokens>(a: Option<T>, b: Option<T>) -> syn::Result<Option<T>> {
+            match (a, b) {
+                (None, None) => Ok(None),
+                (Some(val), None) | (None, Some(val)) => Ok(Some(val)),
+                (Some(a), Some(b)) => {
+                    let mut error = syn::Error::new_spanned(a, "redundant attribute argument");
+                    error.combine(syn::Error::new_spanned(b, "note: first one here"));
+                    Err(error)
+                }
+            }
+        }
+
+        Ok(Self { name: either(self.name, other.name)?, vis: either(self.vis, other.vis)? })
+    }
 }
 
 impl Parse for GetterMeta {
     fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
         let arg_name: Ident = input.parse()?;
-        if arg_name != "name" {
-            return Err(syn::Error::new_spanned(
+        if arg_name == "name" {
+            let _: Token![=] = input.parse()?;
+            let name = input.parse()?;
+
+            Ok(Self { name: Some(name), vis: None })
+        } else if arg_name == "vis" {
+            let _: Token![=] = input.parse()?;
+            let vis = input.parse()?;
+
+            Ok(Self { name: None, vis: Some(vis) })
+        } else {
+            Err(syn::Error::new_spanned(
                 arg_name,
-                "unsupported getter attribute, expected `name`",
-            ));
+                "unsupported getter attribute, expected `name` or `vis`",
+            ))
         }
-
-        let _: Token![=] = input.parse()?;
-        let name = input.parse()?;
-
-        Ok(Self { name: Some(name) })
     }
 }
